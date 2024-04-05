ec6f0cccdc950240007bfb2de440e589a1e38114
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2024-04-05 17:18:32 +0100

    Changed color on hover for Download Cv button

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
                         <a href='#'>
                             <FaEnvelope className='h-10 w-10 text-slate-200 hover:text-orange-500 duration-300'/>
                         </a>
-                        <button class="bg-orange-500 hover:bg-orange-600 text-gray-800 font-bold  py-2 px-4 rounded inline-flex items-center">
+                        <button class="bg-orange-500 hover:bg-slate-200 text-gray-800 font-bold  py-2 px-4 rounded inline-flex items-center">
                             <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                             <span>Download CV</span>
                         </button>
