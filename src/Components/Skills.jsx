40505290fbdaba2cdbdead1c5c797697b37bdd75
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2024-04-19 18:42:51 +0100

    Finished for now with the SKills section, they are nicely aligned on large and mobile screens

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -5,13 +5,29 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <section className='py-20 align-element' id="skills">
+    <section className='py-10 md:py-20 align-element' id="skills">
       <SectionTitle text='tech-stack'/>
-      <div className='py-16 grid gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 content-center'>
-        {skills.map((skill) => {
-          return <SkillsCard key={skill.id} {...skill}/>
+      <div className='py-6 md:py-16 flex flex-wrap justify-center gap-4 '>
+        {skills.map((skill, index) => {
+          return <div key={skill.id} className='skill-item'>
+          <SkillsCard {...skill} />
+        </div>
         })}
       </div>
+      <style jsx>{`
+        .skill-item {
+          margin-bottom: 1rem;
+          flex-basis: calc(33.333% - 1rem);
+          width: calc(33.333% - 1rem); /* Fixed width for larger screens */
+        }
+
+        @media (max-width: 768px) {
+          .skill-item {
+            flex-basis: calc(50% - 1rem);
+            width: calc(50% - 1rem); /* Adjusted width for smaller screens */
+          }
+        }
+      `}</style>
     </section>
   )
 }
