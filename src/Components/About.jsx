8e9607ce5f76bdbe6e25b036e39ea828f6b33036
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2024-04-19 23:03:12 +0100

    About section almost finished, managed to align the p div and svg img on the same line, need to work on mobile layout and About me h2

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,18 +1,23 @@
 import SectionTitle from './SectionTitle'
 import React from 'react'
+import splashImg from '../assets/splashAbout.svg'
 
 const About = () => {
   return (
     <section className='bg-black py-20' id='about'>
-        <div className='align-element grid md:grid-cols-2 items-center '>
+        <div>
             <article>
-                <SectionTitle text='about me'/>
-                <p className='text-slate-200 mt-8 leading-loose'>
-                I'm a full-stack developer specializing in PHP for back-end development and JavaScript for front-end. My expertise includes working with JavaScript, PHP, frameworks like React and Slim, programming model such as OOP, technology stacks such as LAMP and MERN , RESTful API and MVC design pattern. Also worked with Node.js, MongoDB, MySQL and NOSQL , PHPUnit and Jest for testing. Experienced in working with HTML5 and CSS, Bootstrap and Tailwind for browser styling. Worked collaboratively in team environments, following Agile methodologies. Eager to contribute my knowledge to creating innovative websites and applications in the IT industry
+            <div className="border-b border-gray-200 pb-5 w-1/2" >
+              <h2 className="text-3xl font-medium tracking-wider capitalize text-orange-500">About me</h2>
+          </div>
+            <div className='align-element flex flex-wrap justify-between'>
+                <p className='text-slate-200 text-lg mt-8 leading-loose w-1/2 '>
+                I am a passionate full-stack developer who is always looking to innovate and create meaningful solutions. I had the opportunity to develop my skills in languages like HTML, CSS, JavaScript, PHP, Typescript and MySQL at iO Academy, where I joined their award-winning bootcamp Full Stack Track Course. I also used frameworks like REACT and Slim to build projects by following Agile methodologies to work in a collaborative team environment. For a better glimpse of my work, please explore my projects below.
                 </p>
-            </article>
+                <img src={splashImg} className='h-80 lg:h-96'/>
+              </div>
+                </article>
         </div>
-      
     </section>
   )
 }
--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -1,7 +1,7 @@
 
-const SectionTitle = ({text}) => {
+const SectionTitle = ({text, width}) => {
     return (
-        <div className="border-b border-gray-200 pb-5">
+        <div className="border-b border-gray-200 pb-5" style={{width: width}}>
             <h2 className="text-3xl font-medium tracking-wider capitalize text-orange-500 ">{text}</h2>
         </div>
     )
