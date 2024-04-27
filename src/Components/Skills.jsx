7ce37b531700b50d1a53e250913a8bed6fda9a6e
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2024-04-27 15:27:03 +0100

    Adjusted font-size on skills section for mobile version

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,9 +7,9 @@ const Skills = () => {
   return (
     <section className='py-10 md:py-20 align-element' id="skills">
       <SectionTitle text='tech-stack'/>
-      <div className='py-6 md:py-16 flex flex-wrap justify-center gap-4 '>
+      <div className='py-6 md:py-16 flex flex-wrap justify-center gap-4'>
         {skills.map((skill, index) => {
-          return <div key={skill.id} className='skill-item'>
+          return <div key={skill.id} className='skill-item text base lg:text-base text-sm'>
           <SkillsCard {...skill} />
         </div>
         })}
