99c213d0829c97e8cdf2b236b792773ce0b7c2b2
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2024-05-03 19:53:21 +0100

    Added details to the Project cards component

--- src/Components/ProjectsCard.jsx
+++ src/Components/ProjectsCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { FaGithubSquare,FaLinkedin,FaEnvelope } from 'react-icons/fa';
 import { TbWorldWww } from 'react-icons/tb';
-const ProjectsCard = ({url,img,github,title,text,text2,text3,text4,text5,text6}) => {
+const ProjectsCard = ({url,img,gitUrl,title,text,text2,text3,text4,text5,text6}) => {
   const [showDetails,setShowDetails] = useState(false)
   return (
     <article className='bg-black rounded-lg shadow-md hover:shadow-xl duration-300 border border-zinc-400 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 cursor-pointer shadow-zinc-400 hover:shadow-zinc-500'>
@@ -24,7 +24,7 @@ const ProjectsCard = ({url,img,github,title,text,text2,text3,text4,text5,text6})
           <a href={url}>
             <TbWorldWww className='h-8 w-8 text-slate-200 hover:text-orange-500 duration-300'/>
           </a>
-          <a href={url}>
+          <a href={gitUrl}>
             <FaGithubSquare className='h-8 w-8 text-orange-500 hover:text-slate-200 duration-300'/>
           </a>
         </div>
