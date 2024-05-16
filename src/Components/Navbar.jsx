8c57491176d5450bcde8fc6d45dc88ef8fefb413
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2024-05-16 18:18:47 +0100

    Created a nice animation for hamburger menu, need to add the navbar button when clicking on it

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,69 +1,142 @@
-import {links} from '../data'
-import CosminLogo from '../assets/CosminLogo.svg'
 import { useEffect, useRef, useState } from 'react';
-
+import CosminLogo from '../assets/CosminLogo.svg';
+import { links } from '../data';
+import { motion, MotionConfig } from 'framer-motion';
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuRef = useRef(null);
+    const [active, setActive] = useState(false)
 
-const [isMenuOpen, setIsMenuOpen] = useState(false);
-const menuRef = useRef(null);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
-const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-};
+    const handleMenuButtonClick = (e) => {
+        e.stopPropagation();
+        toggleMenu();
+    };
 
-const handleMenuButtonClick = (e) => {
-    e.stopPropagation();
-    toggleMenu();
-}
-const handleCLickOutside = (e) => {
-    if(menuRef.current && !menuRef.current.contains(e.target)){
-        setIsMenuOpen(false)
-    }
-};
+    const handleCLickOutside = (e) => {
+        if (!menuRef.current || !menuRef.current.contains(e.target)) {
+            setIsMenuOpen(false);
+        }
+    };
 
-useEffect(() => {
-    document.addEventListener('click',handleCLickOutside);
-    return () => {
-        document.removeEventListener('click',handleCLickOutside);
-    }
-},[]);
+    useEffect(() => {
+        document.addEventListener('click', handleCLickOutside);
+        return () => {
+            document.removeEventListener('click', handleCLickOutside);
+        };
+    }, []);
 
     return (
-        <nav className="bg-black font-bold text-orange-500 font-sans flex justify-between items-center px-8 py-4">
+        <nav className="bg-black font-bold text-orange-500 font-sans flex justify-between items-center px-8 py-4 relative">
             <div>
-                <img src={CosminLogo} className='h-60 w-auto'/>
+                <img src={CosminLogo} className="h-60 w-auto" />
             </div>
-            {/* <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:item-center sm:py-8"></div> */}
             <div className="hidden sm:flex gap-x-3">
-                {links.map((link)=>{
-                const {id,href,text} = link 
-                return <a key={id} href={href} className='capitalize text-lg tracking-wide hover:text-slate-200 duration-300 px-2'>
-                    {text}
-                </a>
-            })}
-            </div>
-            <button className='sm:hidden' onClick={toggleMenu}>
-            <svg className="h-8 w-8 fill-current text-white" viewBox="0 0 24 24" onClick={handleMenuButtonClick}>
-                    <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm16 4H4v2h16v-2z"
-                    />
-                </svg>
-            </button>
-            {isMenuOpen && (
-                <div ref={menuRef} className="sm:hidden absolute top-16 right-8 bg-black rounded-lg shadow-lg py-2 px-4">
                 {links.map((link) => {
                     const { id, href, text } = link;
                     return (
-                        <a key={id} href={href} className='block capitalize text-lg tracking-wide hover:text-slate-200 duration-300' onClick={handleMenuButtonClick}>
+                        <a
+                            key={id}
+                            href={href}
+                            className="capitalize text-lg tracking-wide hover:text-slate-200 duration-300 px-2"
+                        >
                             {text}
                         </a>
                     );
-                    })}
+                })}
+            </div>
+            <div className="sm:hidden">
+                <MotionConfig   transition={{
+                            duration: 0.5,
+                            ease:"easeInOut"
+                        }}>
+                <motion.button
+                initial={false}
+                onClick={() => setActive((pv) => !pv)}
+                 className="relative h-20 w-20 rounded-full bg-white/transition-colors"
+                 animate={active ? "open" : "closed"}>
+                    <motion.span style={{
+                        left:"50%", 
+                        top:"35%", 
+                        x: "-50%",
+                         y:"-50%"
+                    }} className='absolute h-1 w-10 bg-white'
+                        variants={{
+                            open: {
+                                rotate: ["0deg","0deg","45deg"],
+                                top: ["35%","50%","50%"],
+                            }, 
+                            closed: {
+                                rotate: ["45deg","0deg","0deg"],
+                                top: ["50%","50%","35%"],
+                            },
+                        }}
+                    />
+                    <motion.span style={{
+                        left:"50%", 
+                        top:"50%", 
+                        x: "-50%",
+                         y:"-50%"
+                    }} className='absolute h-1 w-10 bg-white'
+                    variants={{
+                        open: {
+                            rotate: ["0deg","0deg","-45deg"],
+                        }, 
+                        closed: {
+                            rotate: ["-45deg","0deg","0deg"],
+                        },
+                    }}
+                      
+                    />
+                    <motion.span style={{
+                        left:"calc(50% + 10px)", 
+                        bottom:"35%", 
+                        x: "-50%",
+                         y:"50%"
+                    }} className='absolute h-1 w-5 bg-white'
+                    variants={{
+                        open: {
+                            rotate: ["0deg","0deg","45deg"],
+                            left: "50%",
+                            bottom:["35%", "50%","50%"],
+                        }, 
+                        closed: {
+                            rotate: ["45deg","0deg","0deg"],
+                            left:"calc(50% + 10px)",
+                            bottom:["50%","50%","35%"],
+                        },
+                    }}/>
+                </motion.button>
+                </MotionConfig>
+                {isMenuOpen && (
+                    <div
+                        ref={menuRef}
+                        className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center z-50"
+                        onClick={() => setIsMenuOpen(false)}
+                    >
+                        <div className="bg-black bg-opacity-75 rounded-lg shadow-lg py-2 px-4">
+                            {links.map((link) => {
+                                const { id, href, text } = link;
+                                return (
+                                    <a
+                                        key={id}
+                                        href={href}
+                                        className="block capitalize text-xl tracking-wide hover:text-slate-200 duration-300"
+                                        onClick={() => setIsMenuOpen(false)}
+                                    >
+                                        {text}
+                                    </a>
+                                );
+                            })}
+                        </div>
                     </div>
-            )}
+                )}
+            </div>
         </nav>
-    )
-}
-export default Navbar
+    );
+};
+
+export default Navbar;
